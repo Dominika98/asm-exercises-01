32fc0f6532e1e923277b0ce21003427de127b88a
Dominika98 <31960418+Dominika98@users.noreply.github.com>
2018-10-08 11:48:02 +0200

    exercise 4

---
+++ Exercise4/Exercise4v1/Exercise4v1/main.asm
@@ -0,0 +1,14 @@
+;
+; Exercise4v1.asm
+;
+; Created: 10/8/2018 11:42:01 AM
+; Author : Mizia
+;
+
+
+; Replace with your application code
+start:
+    ldi r18, $8 ;	load 8 to r18
+	ldi r19, $b3 ;	load b3 to r19
+	add r18, r19 ;	add r18 and r19
+	nop
