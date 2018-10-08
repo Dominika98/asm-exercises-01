81e375e3f6929c9d45398da0aa28a6473c963d71
Dominika98 <31960418+Dominika98@users.noreply.github.com>
2018-10-08 11:34:23 +0200

    First two exercises

---
+++ Exercise2/Exercise2/main.asm
@@ -0,0 +1,12 @@
+;
+; Exercise2.asm
+;
+; Created: 10/8/2018 10:42:07 AM
+; Author : Mizia
+;
+
+
+; Replace with your application code
+start:
+    ldi r16, 0b0100_0010    ; load value into register r16
+	nop                     ; does nothing, but provides a place to put a breakpoint
---
+++ Exercise3/Exercise3v1/Exercise3v1/main.asm
@@ -0,0 +1,12 @@
+;
+; Exercise3v1.asm
+;
+; Created: 10/8/2018 11:32:15 AM
+; Author : Mizia
+;
+
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
